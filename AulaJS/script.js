d25ec3f1304964c86fb40ba19df6f31e19e4c6ba
Felipe Tozadori Silva <ftozadori@gmail.com>
2025-03-17 22:26:07 -0300

    aula js

---
+++ AulaJS/script.js
@@ -0,0 +1,21 @@
+/*console.log(2+2);
+console.log("Hello World !!!");
+
+var primeiroTipo;
+let segundoTipo;
+const terceiroTipo = 10;
+
+let nome = "Felipe";
+let idade = "21";
+
+console.log(nome);
+console.log(idade);
+
+nome = idade;
+
+console.log(nome);*/
+
+function hello(){
+    document.getElementById('texto').textContent = "hello world.";
+    document.getElementById('texto').style.color = "red";
+}
